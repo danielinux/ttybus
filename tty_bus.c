ba8ff2a1f4ca4a1436e10fe969de158ffacbddae
Frank Sautter <github@sautter.com>
2020-09-27 22:10:14 +0200

    reformatted code

--- tty_bus.c
+++ tty_bus.c
@@ -1,262 +1,244 @@
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdlib.h>
 #include <netinet/in.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/poll.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <sys/time.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/un.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
+#include <time.h>
 #include <unistd.h>
-#include <signal.h>
+
 #include "configure.h"
 
 
-#define MAX_TTY 256
-#define BUFFER_SIZE 4096
+#define MAX_TTY        256
+#define BUFFER_SIZE    4096
 #define POLL_R_TIMEOUT 100
 #define POLL_W_TIMEOUT 50
 static char *tty_bus_path = NULL;
 
-void exiting(void)
-{
-	unlink(tty_bus_path);
+
+void exiting(void) {
+  unlink(tty_bus_path);
 }
 
-void signaled(int signo) 
-{
-	exit(0);
+
+void signaled(int signo) {
+  exit(0);
 }
 
-int bus_init(char *path)
-{
-	struct sockaddr_un sun;
-	int connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
-	memset(&sun, 0, sizeof(struct sockaddr_un));
-	sun.sun_family = AF_UNIX;
-	strncpy(sun.sun_path, path, strlen(path));
-	if(bind(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0){
-		if((errno == EADDRINUSE)){
-			printf( "Could not bind to socket '%s': %s\n", path, strerror(errno));
-			exit(-1);
-		}
-		else if(bind(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0){
-			printf( "Could not bind to socket '%s' (second attempt): %s", path, strerror(errno));
-			exit(-1);
-		}
-	} 
-	chmod(sun.sun_path,0777);
-	if(listen(connect_fd, 1) < 0){
-		printf("Could not listen on fd %d: %s", connect_fd, strerror(errno));
-		exit(-1);
-	}
-	return connect_fd;
+
+int bus_init(char *path) {
+  struct sockaddr_un sun;
+  int connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+  memset(&sun, 0, sizeof(struct sockaddr_un));
+  sun.sun_family = AF_UNIX;
+  strncpy(sun.sun_path, path, strlen(path));
+  if (bind(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0) {
+    if ((errno == EADDRINUSE)) {
+      printf("Could not bind to socket '%s': %s\n", path, strerror(errno));
+      exit(-1);
+    } else if (bind(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0) {
+      printf("Could not bind to socket '%s' (second attempt): %s", path, strerror(errno));
+      exit(-1);
+    }
+  }
+  chmod(sun.sun_path, 0777);
+  if (listen(connect_fd, 1) < 0) {
+    printf("Could not listen on fd %d: %s", connect_fd, strerror(errno));
+    exit(-1);
+  }
+  return connect_fd;
 }
 
 
 void bus_destroy(int fd, char *path) {
-	close(fd);
-	unlink(path);
-
-}
-
-static void usage(char *app)
-{
-	fprintf(stderr,"%s, Ver %s.%s.%s\n", basename(app), MAJORV, MINORV, SVNVERSION);
-	fprintf(stderr,"Usage: %s [-h] [-s bus_path]\n", app);
-	fprintf(stderr,"-h: shows this help\n");
-	fprintf(stderr,"-s bus_path: uses bus_path as bus path name (default: /tmp/ttybus)\n");
-	exit(2);
+  close(fd);
+  unlink(path);
 }
 
 
-int prepare_poll(int *tty, struct pollfd **ppfd, int lfd, int flags){
-	struct pollfd *pfd = (struct pollfd *) *ppfd;
-	int i, fdcount = 0;
-	if (lfd != -1)
-		pfd[fdcount++].fd = lfd;
-
-	for (i = 0; i < MAX_TTY; i++) {
-		if (tty[i] != -1) {
-			pfd[fdcount++].fd = tty[i];
-		}
-	}	
-	for (i = 0; i < fdcount; i++) {
-		pfd[i].events = flags;
-	}
-	return fdcount;
+static void usage(char *app) {
+  fprintf(stderr, "%s, Ver %s.%s.%s\n", basename(app), MAJORV, MINORV, SVNVERSION);
+  fprintf(stderr, "Usage: %s [-h] [-s bus_path]\n", app);
+  fprintf(stderr, "-h: shows this help\n");
+  fprintf(stderr, "-s bus_path: uses bus_path as bus path name (default: /tmp/ttybus)\n");
+  exit(2);
 }
 
 
+int prepare_poll(int *tty, struct pollfd **ppfd, int lfd, int flags) {
+  struct pollfd *pfd = (struct pollfd *) *ppfd;
+  int i, fdcount = 0;
+  if (lfd != -1)
+    pfd[fdcount++].fd = lfd;
 
-void init_dev_array(int **ptty)
-{
-	int i;
-	int *tty = *ptty;
-	for (i = 0; i < MAX_TTY; i++)
-		tty[i] = -1;
-}
+  for (i = 0; i < MAX_TTY; i++) {
+    if (tty[i] != -1)
+      pfd[fdcount++].fd = tty[i];
+  }
 
-int check_poll_errors(struct pollfd *pfd, int n, int *tty)
-{
-	int i,j;
-	int err = 0;
-	for (i = 0; i < n; i++) {
-		if (pfd[i].revents & POLLHUP || pfd[i].revents & POLLERR || pfd[i].revents & POLLNVAL) {
-			for(j = 0; j < MAX_TTY; j++)
-			{
-				if (tty[j] == pfd[i].fd) {
-					close(tty[j]);
-					tty[j] = -1;
-					++err;
-					break;
-				}
-			}
-		}
-	}
-	return err;
+  for (i = 0; i < fdcount; i++)
+    pfd[i].events = flags;
+
+  return fdcount;
 }
 
 
-void recvbuff(int src, char *buf, int size, int *tty)
-{
-	struct pollfd *wpfd;
-	int n, i, w;
-	int pollret;
-
-	wpfd = (struct pollfd*) malloc(sizeof(struct pollfd) * MAX_TTY);
-	if (!wpfd){
-		fprintf(stderr, "alloc error: %s\n", strerror(errno));
-	}	 
-	
-	n = prepare_poll(tty, (struct pollfd **) &wpfd, -1, POLLOUT|POLLHUP);
-	printf("Writing to %d clients: %d bytes\n", n, size);
-	if (n > 1) {
-		pollret = poll(wpfd, n, POLL_W_TIMEOUT);
-		if (pollret < 0) {		
-			fprintf(stderr, "Poll error: %s\n", strerror(errno));
-			sleep(1);
-			return;
-		}
-		(void)check_poll_errors(wpfd, n, tty);
-			
-		for(i = 0; i < n; i++) {
-			if (wpfd[i].revents & POLLOUT && wpfd[i].fd != src) {
-				w = write(wpfd[i].fd, buf, size);
-			}
-		}
-	}
-	free(wpfd);
+void init_dev_array(int **ptty) {
+  int i;
+  int *tty = *ptty;
+  for (i = 0; i < MAX_TTY; i++)
+    tty[i] = -1;
 }
 
 
-int main(int argc, char *argv[])
-{
-	int n = 0;
-	int listenfd = -1;
-	int i, r = 0;
-	int pollret;
-	struct pollfd *pfd;
-	int *tty;
-	char buffer[BUFFER_SIZE];
-
-	pfd = (struct pollfd *)malloc(sizeof(struct pollfd) * (1 + MAX_TTY));
-	tty = (int *)malloc(sizeof(int) * MAX_TTY);
-	if (!pfd || !tty){
-		fprintf(stderr, "alloc error: %s\n", strerror(errno));
-		exit(4);
-	}	 
-	while (1) {
-		int c;
-		c = getopt (argc, argv, "hs:");
-		if (c == -1)
-			break;
-
-		switch (c) {
-			case 'h':
-				usage(argv[0]); //implies exit
-				break;
-
-			case 's':
-				tty_bus_path=strdup(optarg);
-				break;
-
-			default:
-				usage(argv[0]); //implies exit
-		}
-	} 
-	if (optind < argc)
-		usage(argv[0]); //implies exit
-
-	if (!tty_bus_path)
-		tty_bus_path = strdup("/tmp/ttybus");
-	
-	fprintf(stderr,"Creating bus: %s\n", tty_bus_path);
-
-	atexit(exiting);
-	sigset(SIGTERM, signaled);
-	sigset(SIGINT, signaled);
-
-
-	init_dev_array((int **)&tty);
-	listenfd = bus_init(tty_bus_path);
-	if (listenfd < 0) {
-		fprintf(stderr, "Cannot bind to %s: %s\n", tty_bus_path, strerror(errno));
-		exit(1);
-	}
-	for(;;) {
-		n = prepare_poll(tty, (struct pollfd **)&pfd, listenfd, POLLIN|POLLHUP);
-		pollret = poll(pfd, n, POLL_R_TIMEOUT);
-		if (pollret < 0) {
-			fprintf(stderr, "Poll error: %s, n is %d\n", strerror(errno),n);
-			sleep(1);
-			continue;
-
-
-		}
-		if (check_poll_errors(pfd, n, tty) > 0)
-			continue;
-
-		if (pfd[0].revents&POLLIN) {
-			struct sockaddr_un cliaddr;
-			size_t len = sizeof(struct sockaddr_un);
-			int connfd = accept(listenfd, (struct sockaddr *)&cliaddr,&len);
-			if(connfd==-1){
-				bus_destroy(listenfd, tty_bus_path);
-				listenfd = bus_init(tty_bus_path);
-			} else {
-				for(i = 0; i < MAX_TTY; i++)
-					if (tty[i] == -1) {
-						tty[i] = connfd;
-						break;
-					}
-			}
-			continue;
-		}
-		if(pollret > 0) {
-			for(;;) {
-				if (pfd[i].revents&POLLIN) {
-					r = read(pfd[i].fd, buffer, BUFFER_SIZE);
-					if (r > 0)
-						recvbuff(pfd[i].fd, buffer, r, tty);
-					break;
-				}
-				if (++i >= n) {
-					i = 1;
-				}
-			}
-		}
-	}
+int check_poll_errors(struct pollfd *pfd, int n, int *tty) {
+  int i, j;
+  int err = 0;
+  for (i = 0; i < n; i++) {
+    if (pfd[i].revents & POLLHUP || pfd[i].revents & POLLERR || pfd[i].revents & POLLNVAL) {
+      for (j = 0; j < MAX_TTY; j++) {
+        if (tty[j] == pfd[i].fd) {
+          close(tty[j]);
+          tty[j] = -1;
+          ++err;
+          break;
+        }
+      }
+    }
+  }
+  return err;
 }
 
+
+void recvbuff(int src, char *buf, int size, int *tty) {
+  struct pollfd *wpfd;
+  int n, i, w;
+  int pollret;
+
+  wpfd = (struct pollfd *) malloc(sizeof(struct pollfd) * MAX_TTY);
+  if (!wpfd)
+    fprintf(stderr, "alloc error: %s\n", strerror(errno));
+
+
+  n = prepare_poll(tty, (struct pollfd **) &wpfd, -1, POLLOUT | POLLHUP);
+  printf("Writing to %d clients: %d bytes\n", n, size);
+  if (n > 1) {
+    pollret = poll(wpfd, n, POLL_W_TIMEOUT);
+    if (pollret < 0) {
+      fprintf(stderr, "Poll error: %s\n", strerror(errno));
+      sleep(1);
+      return;
+    }
+    (void) check_poll_errors(wpfd, n, tty);
+
+    for (i = 0; i < n; i++) {
+      if (wpfd[i].revents & POLLOUT && wpfd[i].fd != src)
+        w = write(wpfd[i].fd, buf, size);
+    }
+  }
+  free(wpfd);
+}
+
+
+int main(int argc, char *argv[]) {
+  int n = 0;
+  int listenfd = -1;
+  int i, r = 0;
+  int pollret;
+  struct pollfd *pfd;
+  int *tty;
+  char buffer[BUFFER_SIZE];
+
+  pfd = (struct pollfd *) malloc(sizeof(struct pollfd) * (1 + MAX_TTY));
+  tty = (int *) malloc(sizeof(int) * MAX_TTY);
+  if (!pfd || !tty) {
+    fprintf(stderr, "alloc error: %s\n", strerror(errno));
+    exit(4);
+  }
+  while (1) {
+    int c;
+    c = getopt(argc, argv, "hs:");
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 'h':
+        usage(argv[0]);  // implies exit
+        break;
+      case 's':
+        tty_bus_path = strdup(optarg);
+        break;
+      default:
+        usage(argv[0]);  // implies exit
+    }
+  }
+  if (optind < argc)
+    usage(argv[0]);  // implies exit
+
+  if (!tty_bus_path)
+    tty_bus_path = strdup("/tmp/ttybus");
+
+  fprintf(stderr, "Creating bus: %s\n", tty_bus_path);
+
+  atexit(exiting);
+  sigset(SIGTERM, signaled);
+  sigset(SIGINT, signaled);
+
+  init_dev_array((int **) &tty);
+  listenfd = bus_init(tty_bus_path);
+  if (listenfd < 0) {
+    fprintf(stderr, "Cannot bind to %s: %s\n", tty_bus_path, strerror(errno));
+    exit(1);
+  }
+  for (;;) {
+    n = prepare_poll(tty, (struct pollfd **) &pfd, listenfd, POLLIN | POLLHUP);
+    pollret = poll(pfd, n, POLL_R_TIMEOUT);
+    if (pollret < 0) {
+      fprintf(stderr, "Poll error: %s, n is %d\n", strerror(errno), n);
+      sleep(1);
+      continue;
+    }
+    if (check_poll_errors(pfd, n, tty) > 0)
+      continue;
+
+    if (pfd[0].revents & POLLIN) {
+      struct sockaddr_un cliaddr;
+      size_t len = sizeof(struct sockaddr_un);
+      int connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len);
+      if (connfd == -1) {
+        bus_destroy(listenfd, tty_bus_path);
+        listenfd = bus_init(tty_bus_path);
+      } else {
+        for (i = 0; i < MAX_TTY; i++)
+          if (tty[i] == -1) {
+            tty[i] = connfd;
+            break;
+          }
+      }
+      continue;
+    }
+    if (pollret > 0) {
+      for (;;) {
+        if (pfd[i].revents & POLLIN) {
+          r = read(pfd[i].fd, buffer, BUFFER_SIZE);
+          if (r > 0)
+            recvbuff(pfd[i].fd, buffer, r, tty);
+          break;
+        }
+        if (++i >= n)
+          i = 1;
+      }
+    }
+  }
+}
--- tty_fake.c
+++ tty_fake.c
@@ -1,29 +1,26 @@
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/poll.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
 #include <sys/time.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/un.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
+#include <time.h>
 #include <unistd.h>
+
 #include "configure.h"
 
 
-#define MAX_TTY 256
-#define BUFFER_SIZE 4096
+#define MAX_TTY        256
+#define BUFFER_SIZE    4096
 #define POLL_R_TIMEOUT 100
 #define POLL_W_TIMEOUT 50
 
@@ -32,153 +29,146 @@ static char *tty_bus_path;
 static char *ttybak;
 static int force_overwrite = 0;
 
-static void usage(char *app)
-{
-	fprintf(stderr,"%s, Ver %s.%s.%s\n", basename(app), MAJORV, MINORV, SVNVERSION);
-	fprintf(stderr,"Usage: %s [-h] [-s bus_path] tty_device\n", app);
-	fprintf(stderr,"-h: shows this help\n");
-	fprintf(stderr,"-s bus_path: uses bus_path as bus path name (default: /tmp/ttybus)\n");
-	fprintf(stderr,"-o: temporarly backup tty_device to tty_device.bak, if it exists, and restore the original file at exit\n");
-	exit(2);
+
+static void usage(char *app) {
+  fprintf(stderr, "%s, Ver %s.%s.%s\n", basename(app), MAJORV, MINORV, SVNVERSION);
+  fprintf(stderr, "Usage: %s [-h] [-s bus_path] tty_device\n", app);
+  fprintf(stderr, "-h: shows this help\n");
+  fprintf(stderr, "-s bus_path: uses bus_path as bus path name (default: /tmp/ttybus)\n");
+  fprintf(stderr, "-o: temporarly backup tty_device to tty_device.bak, if it exists, and restore the original file at exit\n");
+  exit(2);
 }
 
-int tty_connect(char *path)
-{
-	struct sockaddr_un sun;
-	int connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
-	memset(&sun, 0, sizeof(struct sockaddr_un));
-	sun.sun_family = AF_UNIX;
-	strncpy(sun.sun_path, path, strlen(path));
-	if(connect(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0){
-		perror("Cannot connect to socket");
-		exit(-1);
-	}
-	return connect_fd;
+int tty_connect(char *path) {
+  struct sockaddr_un sun;
+  int connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+  memset(&sun, 0, sizeof(struct sockaddr_un));
+  sun.sun_family = AF_UNIX;
+  strncpy(sun.sun_path, path, strlen(path));
+  if (connect(connect_fd, (struct sockaddr *) &sun, sizeof(sun)) < 0) {
+    perror("Cannot connect to socket");
+    exit(-1);
+  }
+  return connect_fd;
 }
+
 
 static void _tty_restore(void) {
-	fprintf(stderr, "Restoring original device\n");
-	unlink(ttyfake);
-	link(ttybak, ttyfake);
-	unlink(ttybak);
+  fprintf(stderr, "Restoring original device\n");
+  unlink(ttyfake);
+  link(ttybak, ttyfake);
+  unlink(ttybak);
 }
 
-static void tty_restore(int __attribute__((unused)) signo)
-{
-	exit(0);
+
+static void tty_restore(int __attribute__((unused)) signo) {
+  exit(0);
 }
 
-int main(int argc,char *argv[])
-{
 
-	int fd; 
-	struct pollfd pfd[2];
-	int pollret, r, w;
-	char buffer[BUFFER_SIZE];
-	char *pts;
-	int ptmx;
+int main(int argc, char *argv[]) {
+  int fd;
+  struct pollfd pfd[2];
+  int pollret, r, w;
+  char buffer[BUFFER_SIZE];
+  char *pts;
+  int ptmx;
 
-	while (1) {
-		int c;
-		c = getopt (argc, argv, "hos:");
-		if (c == -1)
-			break;
+  while (1) {
+    int c;
+    c = getopt(argc, argv, "hos:");
+    if (c == -1)
+      break;
 
-		switch (c) {
-			case 'h':
-				usage(argv[0]); //implies exit
-				break;
+    switch (c) {
+      case 'h':
+        usage(argv[0]);  // implies exit
+        break;
+      case 's':
+        tty_bus_path = strdup(optarg);
+        break;
+      case 'o':
+        force_overwrite = 1;
+        break;
+      default:
+        usage(argv[0]);  // implies exit
+    }
+  }
+  if (optind != (argc - 1))
+    usage(argv[0]);  // implies exit
 
-			case 's':
-				tty_bus_path=strdup(optarg);
-				break;
-			case 'o':
-				force_overwrite = 1;
-				break;
+  ttyfake = strdup(argv[optind]);
+  if (access(ttyfake, W_OK) == 0) {
+    if (force_overwrite) {
+      ttybak = malloc(strlen(ttyfake) + 5);
+      sprintf(ttybak, "%s.bak", ttyfake);
+      unlink(ttybak);
+      link(ttyfake, ttybak);
+      unlink(ttyfake);
+      atexit(_tty_restore);
+      sigset(SIGTERM, tty_restore);
+      sigset(SIGINT, tty_restore);
+      sigset(SIGUSR1, tty_restore);
+      sigset(SIGUSR2, tty_restore);
+    } else {
+      fprintf(stderr, "%s already exists! use -o to force overwrite\n", ttyfake);
+      exit(1);
+    }
+  }
 
-			default:
-				usage(argv[0]); //implies exit
-		}
-	} 
-	if (optind != (argc -1))
-		usage(argv[0]); //implies exit
-	ttyfake = strdup(argv[optind]);
-	if (access(ttyfake,W_OK) == 0) {
-		if (force_overwrite) {
-			ttybak = malloc(strlen(ttyfake) + 5);
-			sprintf(ttybak, "%s.bak",ttyfake);
-			unlink(ttybak);
-			link(ttyfake,ttybak);
-			unlink(ttyfake);
-			atexit(_tty_restore);
-			sigset(SIGTERM,tty_restore);
-			sigset(SIGINT,tty_restore);
-			sigset(SIGUSR1,tty_restore);
-			sigset(SIGUSR2,tty_restore);
-		} else {
-			fprintf(stderr, "%s already exists! use -o to force overwrite\n", ttyfake);
-			exit(1);
-		}
-	}
-		
+  if (!tty_bus_path)
+    tty_bus_path = strdup("/tmp/ttybus");
 
-	if (!tty_bus_path)
-		tty_bus_path = strdup("/tmp/ttybus");
-	
-	fprintf(stderr,"Connecting to bus: %s\n", tty_bus_path);
-	fd = tty_connect(tty_bus_path);
+  fprintf(stderr, "Connecting to bus: %s\n", tty_bus_path);
+  fd = tty_connect(tty_bus_path);
 
+  ptmx = open("/dev/ptmx", O_RDWR);
+  pts = (char *) ptsname(ptmx);
+  fprintf(stderr, "Device: %s is now %s\n", pts, ttyfake);
+  grantpt(ptmx);
+  unlockpt(ptmx);
 
- 	ptmx = open("/dev/ptmx", O_RDWR);
-	pts = (char*)ptsname(ptmx);
-	fprintf(stderr,"Device: %s is now %s\n", pts, ttyfake);
-	grantpt(ptmx);
-	unlockpt(ptmx);
+  symlink(pts, ttyfake);
+  chmod(pts, 00777);
 
-	symlink(pts,ttyfake);
-	chmod(pts,00777);
+  for (;;) {
+    pfd[0].fd = ptmx;
+    pfd[0].events = POLLIN;
+    pfd[1].fd = fd;
+    pfd[1].events = POLLIN;
+    pollret = poll(pfd, 2, 1000);
+    if (pollret < 0) {
+      fprintf(stderr, "Poll error: %s\n", strerror(errno));
+      exit(1);
+    }
+    if (pollret == 0)
+      continue;
 
-
-	for (;;) {
-		pfd[0].fd = ptmx;
-		pfd[0].events = POLLIN;
-		pfd[1].fd = fd;
-		pfd[1].events = POLLIN;
-		pollret = poll(pfd, 2, 1000);
-		if (pollret < 0) {
-			fprintf(stderr, "Poll error: %s\n", strerror(errno));
-			exit(1);
-		}
-		if (pollret == 0)
-			continue;
-
-		if ( (pfd[0].revents & POLLHUP || pfd[0].revents & POLLERR || pfd[0].revents &POLLNVAL) ||
-			(pfd[1].revents & POLLHUP || pfd[1].revents & POLLERR || pfd[1].revents &POLLNVAL) )
-			exit(1);
-		if (pfd[0].revents & POLLIN) {
-			r = read(ptmx, buffer, BUFFER_SIZE);
-			pfd[1].events = POLLOUT;
-			pollret = poll(&pfd[1], 1, 50);	
-			if (pollret < 0) {
-				fprintf(stderr, "Poll error: %s\n", strerror(errno));
-				exit(1);
-			}
-			if(pfd[1].revents & POLLOUT) {
-				w = write(fd, buffer, r);
-			}
-		}
-		if (pfd[1].revents & POLLIN) {
-			r = read(fd, buffer, BUFFER_SIZE);
-			pfd[0].fd = ptmx;
-			pfd[0].events = POLLOUT;
-			pollret = poll(&pfd[0], 1, 50);	
-			if (pollret < 0) {
-				fprintf(stderr, "Poll error: %s\n", strerror(errno));
-				exit(1);
-			}
-			if(pfd[0].revents & POLLOUT) {
-				w = write(ptmx, buffer, r);
-			}
-		}
-	}		
+    if ((pfd[0].revents & POLLHUP || pfd[0].revents & POLLERR || pfd[0].revents & POLLNVAL) ||
+        (pfd[1].revents & POLLHUP || pfd[1].revents & POLLERR || pfd[1].revents & POLLNVAL))
+      exit(1);
+    if (pfd[0].revents & POLLIN) {
+      r = read(ptmx, buffer, BUFFER_SIZE);
+      pfd[1].events = POLLOUT;
+      pollret = poll(&pfd[1], 1, 50);
+      if (pollret < 0) {
+        fprintf(stderr, "Poll error: %s\n", strerror(errno));
+        exit(1);
+      }
+      if (pfd[1].revents & POLLOUT)
+        w = write(fd, buffer, r);
+    }
+    if (pfd[1].revents & POLLIN) {
+      r = read(fd, buffer, BUFFER_SIZE);
+      pfd[0].fd = ptmx;
+      pfd[0].events = POLLOUT;
+      pollret = poll(&pfd[0], 1, 50);
+      if (pollret < 0) {
+        fprintf(stderr, "Poll error: %s\n", strerror(errno));
+        exit(1);
+      }
+      if (pfd[0].revents & POLLOUT)
+        w = write(ptmx, buffer, r);
+    }
+  }
 }
